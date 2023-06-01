244ef12acb653e4cb203a1bcd52635aee1a1a250
lonefd <104296058+lonefd@users.noreply.github.com>
2023-06-01 23:40:36 +0300

    Dark and light theme

--- src/zrz/js/main.js
+++ src/zrz/js/main.js
@@ -1,3 +1,13 @@
+const themeSwitchers = document.querySelectorAll('.change-theme');
+themeSwitchers.forEach(switcher =>{
+    switcher.addEventListener('click',function() {
+      applyTheme(this.dataset.theme);
+    });
+});
+function applyTheme(themeName) {
+  let themeUrl = `css/theme-${themeName}.css`;
+  document.querySelector('[title="theme"]').setAttribute('href', themeUrl);
+}
 const quizData = [
   {
     question: "What does HTML stand for?",
